e9d8897776c072b883f9c7587b28c68c408177f3
amowzoon <151901183+amowzoon@users.noreply.github.com>
2026-04-05 13:33:21 -0400

    Delete server/tests directory

--- server/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add /app to path so pytest can find processing/, websocket_manager, etc.
-sys.path.insert(0, "/app")
-# Add /app/processing to path so pipeline.py can find segment_by_texture, etc.
-sys.path.insert(0, "/app/processing")
